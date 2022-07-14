ec7d47973d9e8fc15684b3d453d127db9629c620
Kim Kang Min <rkdals133@gmail.com>
2022-07-14 22:29:26 +0900

    go spring boot

---
+++ DBS/src/main/java/com/example/DBS/DbsPdfViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsPdfViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsPdfViewerApplication.class, args);
+	}
+
+}
---
+++ DBS/src/test/java/com/example/DBS/DbsPdfViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsPdfViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
