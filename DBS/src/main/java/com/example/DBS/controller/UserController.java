5e94daf92f028e73431d232bff994d1c26cdad31
Kim Kang Min <rkdals133@gmail.com>
2022-07-19 21:57:16 +0900

    회원가입 기능 구현

--- DBS/src/main/java/com/example/DBS/controller/UserController.java
+++ DBS/src/main/java/com/example/DBS/controller/UserController.java
@@ -3,18 +3,23 @@ package com.example.DBS.controller;
 import com.example.DBS.domain.User;
 import com.example.DBS.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user/signup")
-    public ResponseEntity<UserResponse> signup (@RequestBody User user){
+    @PostMapping("/users")
+    public Long signup (@RequestBody User user){
 
+
+        return userService.signUp(user);
     }
 }
