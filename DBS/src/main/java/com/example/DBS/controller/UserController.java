0177f1a7056702811520cf625f6c29e2aea4a913
Kim Kang Min <rkdals133@gmail.com>
2022-07-21 22:09:05 +0900

    로그인 기능 구현

--- DBS/src/main/java/com/example/DBS/controller/UserController.java
+++ DBS/src/main/java/com/example/DBS/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
 
         } catch (RuntimeException re){
             responseBody.setResultCode(-1);
-            responseBody.setResultMsg("런타임 에러");
+            responseBody.setResultMsg(re.getMessage());
             return ResponseEntity.badRequest().body(responseBody);
         } catch (Exception e){
             responseBody.setResultCode(-2);
@@ -33,6 +33,24 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
         }
 
+        return ResponseEntity.ok().body(responseBody);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity login (@RequestBody User user){
+        CustomResponseBody<User> responseBody = new CustomResponseBody<>("로그인 성공");
+        try {
+            User loginUser = userService.login(user);
+            responseBody.getList().add(loginUser);
+        } catch (RuntimeException re){
+            responseBody.setResultCode(-1);
+            responseBody.setResultMsg(re.getMessage());
+            return ResponseEntity.badRequest().body(responseBody);
+        } catch (Exception e){
+            responseBody.setResultCode(-2);
+            responseBody.setResultMsg(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+        }
         return ResponseEntity.ok().body(responseBody);
     }
 }
